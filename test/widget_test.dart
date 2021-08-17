940223b91242b5a1780ce868782b6bf52c4d6629
ikhwa-beep <85183201+ikhwa-beep@users.noreply.github.com>
2021-08-17 16:41:22 +0800

    Change Test Method

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:food_app/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('finds a Text widget', (WidgetTester tester) async {
+    // Build an App with a Text widget that displays the letter 'H'.
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: Text('H'),
+      ),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Find a widget that displays the letter 'H'.
+    expect(find.text('H'), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('finds a widget using a Key', (WidgetTester tester) async {
+    // Define the test key.
+    const testKey = Key('K');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Build a MaterialApp with the testKey.
+    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+
+    // Find the MaterialApp widget using the testKey.
+    expect(find.byKey(testKey), findsOneWidget);
+  });
+
+  testWidgets('finds a specific instance', (WidgetTester tester) async {
+    const childWidget = Padding(padding: EdgeInsets.zero);
+
+    // Provide the childWidget to the Container.
+    await tester.pumpWidget(Container(child: childWidget));
+
+    // Search for the childWidget in the tree and verify it exists.
+    expect(find.byWidget(childWidget), findsOneWidget);
   });
 }
